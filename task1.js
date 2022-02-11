2a3b3b90ae94db8c34527cd4982984549e125573
Manshree17 <95197811+Manshree17@users.noreply.github.com>
2022-02-11 10:34:19 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,17 @@
+function  red()
+    {
+        document.getElementById('circle').style.background="red";
+    }
+    function  green()
+    {
+        document.getElementById('circle').style.background="green";
+    }
+    function  yellow()
+    {
+        document.getElementById('circle').style.background="yellow";
+    }
+    function  blue()
+    {
+        document.getElementById('circle').style.background="rgb(40, 136, 180)";
+    }
+
